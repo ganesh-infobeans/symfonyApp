7867216758a25addcdd8b76d64cab9f61a393625
ganesh.infobeans <ganesh.patil@infobeans.com>
2015-03-10 20:39:11 +0530

    Form changes

--- php/src/Acme/DemoBundle/Controller/QuestionaryController.php
+++ php/src/Acme/DemoBundle/Controller/QuestionaryController.php
@@ -18,6 +18,8 @@ class QuestionaryController extends Controller
         ->add('q1', 'choice', array('choices' => array('1'=>'YAML','2'=> 'PHP','3'=>'XML','4'=> 'All of above'),'label'=>'In Symfony, the routing configuration can be written in .....'))
         ->add('q2', 'choice', array('choices' => array('1'=>'Bundle','2'=> 'Kernel','3'=>'Controller','4'=>'model'),'label' => ' Which of the following returns Symfony2 Response object back to the user?  '))
         ->add('q3', 'choice', array('choices' => array('1'=>'Response object','2'=> 'Request Object','3'=>'PHP Object ','4'=>'none'),'label' => ' The goal of each controller in Symfony is to return a.....  '))
+        ->add('q4', 'choice', array('choices' => array('1'=>'Bundle','2'=> 'Router','3'=>'COntroller ','4'=>'Model'),'label' => ' Which of the following in Symfony is a directory that houses everything related to a specific feature, including PHP classes, configuration, and
+even stylesheets and Javascript files?.'))
         ->add('save', 'submit', array('label' => 'Submit'))
         ->getForm();
 
@@ -40,6 +42,10 @@ class QuestionaryController extends Controller
                                     $rightAnswer++;
                                     break;
                                 case 'q3' :
+                                    if($val == 1)
+                                    $rightAnswer++;
+                                    break;
+                                case 'q4' :
                                     if($val == 1)
                                     $rightAnswer++;
                                     break;
